833891a473377ac9dd2e47f6da4b31cda01247a4
seventynineBPM <aberrow.joo@gmail.com>
2020-05-06 15:34:34 +0900

    Commit Tree

---
+++ Tree/Tree.playground/Sources/Tree.swift
@@ -0,0 +1,44 @@
+/*
+Reference : https://github.com/raywenderlich/swift-algorithm-club
+*/
+import Foundation
+
+public class TreeNode<T> {
+    public var value: T
+    
+    public weak var parent: TreeNode?
+    public var children = [TreeNode<T>]()
+    
+    public init(value: T) {
+        self.value = value
+    }
+    
+    public func addChild(_ node: TreeNode<T>) {
+        children.append(node)
+        node.parent = self
+    }
+}
+
+extension TreeNode: CustomStringConvertible {
+    public var description: String {
+        var s = "\(value)"
+        if !children.isEmpty {
+            s += " {" + children.map { $0.description }.joined(separator: ", ") + "}"
+        }
+        return s
+    }
+}
+
+extension TreeNode where T: Equatable {
+    public func search(_ value: T) -> TreeNode? {
+        if value == self.value {
+            return self
+        }
+        for node in children {
+            if let found = node.search(value) {
+                return found
+            }
+        }
+        return nil
+    }
+}
